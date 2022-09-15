e12190123c80ec4c855ae9d5d777077be2829441
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-09-15 21:28:12 +0430

    feat : (WIP) add registery service in app

---
+++ src/internal/services/auth/register.go
@@ -0,0 +1,14 @@
+package auth
+
+import (
+	"github.com/mojtabaRKS/link_shortener/internal/webserver/controllers/v1/auth"
+	// "github.com/mojtabaRKS/link_shortener/internal/entities"
+)
+
+func Register(r auth.RegisterRequest) (string, error) {
+	// user := entities.User{}
+
+
+
+	return "user token", nil
+}
